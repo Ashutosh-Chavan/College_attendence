7d5556b49b2da9d47820a9ef678252bc029a5848
Ashutosh-Chavan <ashutoshchavan.9910@gmail.com>
2024-09-02 20:14:09 +0530

    added cc files

---
+++ CC/app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { Image, StyleSheet} from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#FCE38A', dark: '#e7e7b4' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/logo.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome To EduEase!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  reactLogo: {
+    height: 40,
+    width: 190,
+    bottom: 20,
+    left: 20,
+    position: 'absolute',
+  },
+});
